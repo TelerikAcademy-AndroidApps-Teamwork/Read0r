a8f007c301734dd6f348ad7e3bae7c0c41bc30f7
stanevplamen <stanev.plamen@gmail.com>
2014-10-11 15:18:59 +0300

    SQLite beginning

---
+++ app/src/com/example/read0r/Read0rLocalData.java
@@ -0,0 +1,94 @@
+package com.example.read0r;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import com.example.read0r.Interfaces.IDocumentReader;
+import com.example.read0r.Interfaces.ILocalDataHandler;
+import com.example.read0r.Models.ReadableBook;
+
+public class Read0rLocalData implements ILocalDataHandler {
+
+	private final String LOG_TAG = getClass().getSimpleName();
+	private ConnectionSource connectionSource;
+	private Dao<ReadableBook, Integer> readleBooksDoa;
+
+	{
+		if (connectionSource == null) {
+			try {
+				connectionSource =
+						new JdbcConnectionSource(
+								"jdbc:h2:/data/data/com.example.helloandroidh2/databases/helloAndroidH2");
+				readleBooksDoa = DaoManager.createDao(connectionSource, ReadableBook.class);
+			} catch (SQLException e) {
+				throw new RuntimeException("Problems initializing database objects", e);
+			}
+			try {
+				TableUtils.createTable(connectionSource, ReadableBook.class);
+			} catch (SQLException e) {
+				// ignored
+			}
+		}
+	}
+	
+	public ReadableBook getBookById(int id) {
+		
+		List<ReadableBook> results;
+		try {
+			results = readleBooksDoa.queryBuilder().where().eq("id", id).query();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+			return null;
+		}
+		return results.get(0);
+	}	
+	
+	public List<ReadableBook> getBooks() {
+		
+		List<ReadableBook> allBooksList;
+		try {
+			allBooksList = readleBooksDoa.queryForAll();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+			return null;
+		}
+				
+		return allBooksList;
+	}
+	
+	public void addBook(ReadableBook book) {
+		
+		try {
+			readleBooksDoa.create(book);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}	
+}
+
+
+
+
